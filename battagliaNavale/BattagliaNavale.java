5c406734efded529518f61fa9eec97aeabaa5125
311027 <s311027@studenti.polito.it>
2025-10-27 15:31:41 +0100

    main

--- battagliaNavale/BattagliaNavale.java
+++ battagliaNavale/BattagliaNavale.java
@@ -7,9 +7,9 @@ public class BattagliaNavale {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         Board board_Utente=new Board();
-        //Board board_Computer=new Board();
-        //board_Computer.posizionaRandom();
-        //board_Utente.posizionaRabndom();
+        Board board_Computer=new Board();
+        board_Computer.posizionaRandom();
+        board_Utente.posizionaRandom();
         System.out.println("Giochiamo a battaglia navale!");
     }
 }
