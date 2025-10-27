24a6c900a5fde4e6706a9e6a8eab040ec70befed
311027 <s311027@studenti.polito.it>
2025-10-27 15:12:22 +0100

    main

--- battagliaNavale/BattagliaNavale.java
+++ battagliaNavale/BattagliaNavale.java
@@ -1,13 +1,12 @@
-import BattagliaNavale.Board;
-import BattagliaNavale.Nave;
+package Battaglia_Navale.GruppoBlu_BattagliaNavale.BattagliaNavale;
 import java.util.Scanner;
 public class BattagliaNavale {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
-        Board board_Utente=new Board();
-        Board board_Computer=new Board();
-        board_Computer.posizionaRandom();
-        board_Utente.posizionaRabndom();
+        //Board board_Utente=new Board();
+        //Board board_Computer=new Board();
+        //board_Computer.posizionaRandom();
+        //board_Utente.posizionaRabndom();
         System.out.println("Giochiamo a battaglia navale!");
     }
 }
